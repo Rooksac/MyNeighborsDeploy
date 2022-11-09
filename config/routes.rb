85d086974fdf30946d295c418b82fe21a3865f2b
root <root@DESKTOP-NH2VI3D.localdomain>
2022-11-09 15:40:27 -0500

    testing with bundle

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
-  post "/login", to: "users#login"
-  delete "/logout", to: "users#logout"
-  get "/me",  to: "users#me"
-  get "/puzzlehistory", to: "puzzles#puzzlehistory"
-  get "/puzzlefeed", to: "puzzles#puzzlefeed"
+  namespace :api do
+    post "/login", to: "users#login"
+    delete "/logout", to: "users#logout"
+    get "/me",  to: "users#me"
+    get "/puzzlehistory", to: "puzzles#puzzlehistory"
+    get "/puzzlefeed", to: "puzzles#puzzlefeed"
   
-  resources :users, only: [:update, :delete, :create, :show]
-  resources :puzzles, only: [:index, :show, :destroy, :create]
-  resources :clues, only: [:create]
-  resources :attempted_puzzles, only: [:create]
+    resources :users, only: [:update, :delete, :create, :show]
+    resources :puzzles, only: [:index, :show, :destroy, :create]
+    resources :clues, only: [:create]
+    resources :attempted_puzzles, only: [:create]
+  end
+  
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
